df5c8355b39716896970573a45748818fd9fab31
BasCandia <bcandia.0123@gmail.com>
2020-10-14 17:27:43 -0300

    Logre crear las tablas y conectarla a mi base de datos local

---
+++ projectCL/database/migrations/2020_10_14_194826_create_propietarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropietariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propietarios', function (Blueprint $table) {
+            $table->id();
+            $table->String('Nombre');
+            $table->String('Apellido');
+            $table->String('Fono');
+            $table->String('Correo');
+            $table->String('Razon_Social');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('propietarios');
+    }
+}
