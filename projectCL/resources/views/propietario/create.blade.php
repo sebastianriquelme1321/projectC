7ddacc0a9b1af68a5a1900a3ec84294883bb8418
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2020-11-17 00:20:37 -0300

    aplicar adminlte en crear propietarios

--- projectCL/resources/views/propietario/create.blade.php
+++ projectCL/resources/views/propietario/create.blade.php
@@ -1,19 +1,67 @@
+@extends('adminlte::page')
 
-<form action="{{url('/propietarios')}}" method="post" enctype="multipart/form-data">
-    {{csrf_field()}}
-    <label for="Nombre">{{'Nombre'}}</label>
-    <input type="text" name="Nombre" id="Nombre" value="">
-    <br>
-    <label for="Apellido">{{'Apellido'}}</label>
-    <input type="text" name="Apellido" id="Apellido" value="">
-    <br>
-    <br>
-    <label for="Correo">{{'Correo'}}</label>
-    <input type="text" name="Correo" id="Correo" value="">
-    <br>
-    <label for="Fono">{{'Fono'}}</label>
-    <input type="text" name="Fono" id="Fono" value="">
-    <br>
-    <input type="submit" value="Agregar">
-    
-</form>
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+    <form action="{{url('/propietarios')}}" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <section class="content">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card card-primary">
+                  <div class="card-header">
+                    <h3 class="card-title">General</h3>
+      
+                   
+                  </div>
+                  <div class="card-body" style="display: block;">
+                    <div class="form-group">
+                      <label for="Nombre">{{'Nombre'}}</label>
+                      <input type="text" name="Nombre" id="Nombre" value="" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label for="Apellido">{{'Apellido'}}</label>
+                      <input type="text" name="Apellido"id="Apellido" value="" class="form-control" >
+                    </div>
+                    <div class="form-group">
+                        <label for="Correo">{{'Correo'}}</label>
+                        <input type="text" name="Correo"id="Correo" value="" class="form-control" 
+                    </div>
+                    <div class="form-group">
+                        <label for="Fono">{{'Fono'}}</label>
+                        <input type="text" name="Fono"id="Fono" value="" class="form-control" >
+                      </div>
+                   
+                    </div>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              
+                <!-- /.card -->
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <a href="#" class="btn btn-secondary">Cancel</a>
+                <input type="submit" value="Agregar" class="btn btn-success float-right">
+              </div>
+            </div>
+          </section>
+    </form>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
