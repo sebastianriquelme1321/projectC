eb667540fdeccf27fb64f0ceb6fb8492aaf9d64e
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2020-11-17 01:18:15 -0300

    index adminlte

--- projectCL/resources/views/propietario/index.blade.php
+++ projectCL/resources/views/propietario/index.blade.php
@@ -1,49 +1,81 @@
+@extends('adminlte::page')
 
-<table class="table table-light">
+@section('title', 'Dashboard')
 
-    <thead class="thead-light">
-        <tr>
-            <th></th>
-           
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>fono</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
+@section('content_header')
+    <h1>Propietarios</h1>
 
-    <tbody>
-    @foreach($propietarios as $propietario)
-        <tr>
+@stop
+
+@section('content')
+    <div class="row">
+      <div class="col-12"> 
+        <a href="{{url('/propietarios/create')}}" class="btn btn-primary btn-lg float-right" >Agregar</a>
+        
+        
+      </div>
+    </div>
+    <br>
+    <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Propietarios</h3>
+
+
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0" style="height: 700px;">
+                <table class="table table-head-fixed text-nowrap">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>Apellido</th>
+                      <th>Fono</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                 @foreach($propietarios as $propietario)
+                   <tr> 
+                        <td>{{$loop->iteration}}</td>
+                        <td>{{$propietario->Nombre}}</td>
+                        <td>{{$propietario->Apellido}}</td>
+                        <td>{{$propietario->Fono}}</td>
+                        <td>
+                          <form method="post" action="{{url('/propietarios/'.$propietario->ID_prop)}}">
+                          {{csrf_field() }}
+                          {{method_field('GET')}}
+                          <button type="submit" class="btn btn-block btn-success">Ver mas</button>
+          
+                          </form>
+                      </td>
+                      <td>
+                        <a href="{{url('/propietarios/'.$propietario->ID_prop.'/edit')}}">
+                         <button type="submit" class="btn btn-block btn-warning" onclick="return confirm('Editar');">Editar</button>
+                        </a>
+        
+                      </td>
+                      <td>
+                        <form method="post" action="{{url('/propietarios/'.$propietario->ID_prop)}}">
+                        {{csrf_field() }}
+                        {{method_field('DELETE')}}
+                        <button type="submit" class="btn btn-block btn-danger" onclick="return confirm('Borrar');">Borrar</button>
             
-            <td>{{$loop->iteration}}</td>
-           
-            <td>{{$propietario->Nombre}}</td>
-            <td>{{$propietario->Apellido}}</td>
-            <td>{{$propietario->fono}}</td>
-            <td>
-                <form method="post" action="{{url('/propietarios/'.$propietario->ID_prop)}}">
-                {{csrf_field() }}
-                {{method_field('GET')}}
-                <button type="submit">Ver mas</button>
-                    
-                </form>
-            </td>
-            <td>
-                <a href="{{url('/propietarios/'.$propietario->ID_prop.'/edit')}}">
-                 <button type="submit" onclick="return confirm('Editar');">Editar</button>
-                </a>
+                        </form>
+                    </td>
 
-            <td>
-            <form method="post" action="{{url('/propietarios/'.$propietario->ID_prop)}}">
-            {{csrf_field() }}
-            {{method_field('DELETE')}}
-            <button type="submit" onclick="return confirm('Borrar');">Borrar</button>
-                
-            </form>
-        </td>
-        </tr>
-    @endforeach
-    </tbody>
+                   </tr>
+                 @endforeach
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+@stop
 
-</table>
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
