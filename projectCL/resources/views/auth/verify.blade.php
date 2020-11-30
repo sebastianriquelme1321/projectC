c0974c6a77ec027f5abec28fb46ff94f876761d2
Matias-Carrasco <matfcvega@gmail.com>
2020-11-30 16:56:34 -0300

    Cambio de idioma a la vista de verificar correo (Falta hacerlo mas bonito)

--- projectCL/resources/views/auth/verify.blade.php
+++ projectCL/resources/views/auth/verify.blade.php
@@ -5,24 +5,29 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Confirmar Correo Electronico') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ __('Un nuevo enlace de verificacion a sido enviado a ') }}
+                        <strong>{{auth()->user()->email}}</strong>
+
+                    </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Busque en el Buzon de su correo') }} <strong>{{auth()->user()->email}}</strong>
+                    {{ __('un mensaje enviado desde la direccion') }} <Strong>APPRegistroNuevoCollao@gmail.com</Strong>
+                    {{ __('que contiene los datos para la verificacion. ') }}
+                    {{ __('Es posible que el mensaje haya sido enviado a la carpeta SPAM o similar.') }}
+                    <br>
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit"
+                            class="btn btn-link p-0 m-0 align-baseline">{{ __('Enviar un nuevo enlace de verificacion') }}</button>.
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
